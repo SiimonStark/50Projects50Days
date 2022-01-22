c95b923e0f72705cf84118254844e2c38daf5383
SiimonStark <justinpyktel@gmail.com>
2022-01-21 23:25:31 -0800

    add: conditional hover focus

--- P08_Form Wave/public/script.js
+++ P08_Form Wave/public/script.js
@@ -2,15 +2,17 @@ const labels = document.querySelectorAll('.form-control label');
 
 const targetEvents = () => {
   labels.forEach(label => {
-    label.addEventListener('click', () => setFocus(label));
-    label.addEventListener('mouseenter', () => setFocus(label));
+    label.addEventListener('click',
+      () => setFocus(label));
+    label.addEventListener('mouseenter',
+      () => setFocus(label, true));
   });
 }
 
-const setFocus = (label) => {
+const setFocus = (label, hover) => {
   let text = label.innerText.split(' ').join('').toLowerCase();
   let input = document.getElementById(text);
-  if (input.value.length <= 0) {
+  if (!hover || input.value.length <= 0) {
     input.focus();
   }
 }
@@ -20,7 +22,7 @@ const moveTextUp = () => {
     label.innerHTML = label.innerText
       .split(``)
       .map((letter, ind) => {
-        return `<span style="transition-delay:${ind * .50}" > ${letter}</span > `
+        return `<span style="transition-delay:${ind * 30}ms" > ${letter}</span > `
       })
       .join(``);
   });
