4c1c0ef8bb44a186db24120c389d916ce3a01e94
SiimonStark <justinpyktel@gmail.com>
2022-01-24 11:57:50 -0800

    Working

--- P09_Sound Board/public/script.js
+++ P09_Sound Board/public/script.js
@@ -3,7 +3,23 @@ const sounds = ['slide', 'stroke-down', 'stroke-up', 'chords', 'riff', 'strum'];
 sounds.forEach(sound => {
   const btn = document.createElement('button');
   btn.classList.add('btn');
+
   btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  })
 
   document.getElementById('buttons').appendChild(btn);
 })
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
