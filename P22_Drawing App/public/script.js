cc9fe6581683b1bdd2407d9bacc173e867ac0731
SiimonStark <siimonstark@gmail.com>
2022-02-22 17:44:45 -0800

    Script: pt2

--- P22_Drawing App/public/script.js
+++ P22_Drawing App/public/script.js
@@ -2,11 +2,40 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x;
 let y;
 
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false;
+
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+
+        x = x2;
+        y = y2;
+    }
+});
+
 function drawCircle(x, y) {
+    // default canvas circle pulled from docs
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
     ctx.fillStyle = color;
@@ -14,10 +43,11 @@ function drawCircle(x, y) {
 }
 
 function drawLine(x1, y1, x2, y2) {
+    // default canvas line pulled from docs
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size * 2;
+    ctx.lineWidth = size * 2; // *2 so that it === diameter, !== radius
     ctx.stroke();
 }
