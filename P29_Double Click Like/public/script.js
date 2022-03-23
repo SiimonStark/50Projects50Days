9c3e9faa638895ef6ecae53db6756d8789990846
SiimonStark <siimonstark@gmail.com>
2022-03-22 17:42:41 -0700

    Not working

--- P29_Double Click Like/public/script.js
+++ P29_Double Click Like/public/script.js
@@ -1,26 +1,12 @@
-const post = document.querySelector('.post');
+const cards = document.querySelectorAll('.card');
 const times = document.getElementById('times');
 
 let clickTime = 0;
 let heartStyle = '';
 
-// Could have instead used "dblclick"
-post.addEventListener('click', (e) => {
-    if (clickTime === 0) {
-        clickTime = new Date().getTime();
-        console.log(clickTime);
-    } else {
-        if ((new Date().getTime() - clickTime) < 800) {
-            createHeart(e);
+cards.forEach(post => post.addEventListener('dblclick', (e) => createHeart(e, post)));
 
-        } else {
-            clickTime = new Date().getTime();
-        }
-    }
-
-});
-
-const createHeart = (e) => {
+const createHeart = (e, post) => {
     const heart = document.createElement('i');
     heart.classList.add('fas');
     heart.classList.add('fa-heart');
@@ -29,7 +15,11 @@ const createHeart = (e) => {
     const x = e.clientX;
     const y = e.clientY;
 
-    const { offsetLeft, offsetTop } = e.target;
+    console.log(e);
+    const offsetLeft = e.offsetX;
+    const offsetTop = e.offsetY;
+    console.log(x, y);
+    console.log(offsetLeft, offsetTop);
 
     const xInside = x - offsetLeft;
     const yInside = y - offsetTop;
