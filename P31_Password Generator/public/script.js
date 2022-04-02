230f53b3bb8811bd7719b4ef686d4b2a903bacd5
siimonstark <siimonstark@gmail.com>
2022-04-02 11:58:33 -0700

    Script: set character values

--- P31_Password Generator/public/script.js
+++ P31_Password Generator/public/script.js
@@ -4,15 +4,36 @@ const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('length');
 const generatePasswordEl = document.getElementById('generate');
-let password: '';
-
-let upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-let numbers = '0123456789';
-let symbols = '~!@#$%^&*()_-+=?.,;:<>[]{}';
 
 generatePasswordEl.addEventListener('click', generatePassword);
 
 function generatePassword() {
 
+}
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+function getRandomLower() {
+    // Use string object methods to randomize character code within limits
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    // Use string object methods to randomize character code within limits
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    // Use string object methods to randomize character code within limits
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
